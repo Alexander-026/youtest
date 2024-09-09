dac7b68775b9ce55a635c8e30bda9096fbea4eb8
aleksandr026 <alexanderbrendin@gmail.com>
2024-09-09 15:39:22 +0200

    fixed sameSite

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,7 +14,7 @@ const saveCookie = (res, tokenName, token) => {
   res.cookie(tokenName, token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    // sameSite: process.env.NODE_ENV === "production" ? "Lap" : 'None',
+    sameSite: process.env.NODE_ENV === "production" ? "None" : 'Lax',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 }
